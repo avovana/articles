5543a5afb3028a6699a1df37464ae1b4607597c9
vnevzorov <avovana@yandex.ru>
2023-01-12 14:14:23 +0300

    utils: check same words

--- MapReduce/4_merge_by_levels.py
+++ MapReduce/4_merge_by_levels.py
@@ -6,21 +6,19 @@ import argparse
 import sys
 
 
+max_words_in_ram = 500
 counts = dict()
 
 
-def save_to_file(start_depth_level, end_depth_level, filename):
+def save_to_file(start_depth_level, end_depth_level, filename, output_file_postfix):
     # global counts
-    output_file = filename.split("_")[0] + "_depth_third" + "_" + str(start_depth_level) + "-" + str(end_depth_level) + ".txt" #  + "_" + str(file_number) + ".txt"
+    output_file = filename.split("_")[0] + "_" + output_file_postfix + "_" + str(start_depth_level) + "-" + str(end_depth_level) + ".txt" #  + "_" + str(file_number) + ".txt"
     with open(output_file, 'w') as f:
-         for word, count in sorted(counts.items()): # , reverse=True # sort by key
+         for word, count in sorted(counts.items()):
              f.write('%s : %s\n' % (word, count))
 
-        # for word, count in dict(sorted(counts.items(), key=lambda item: item[1], reverse=True)).items():
-        #     f.write('%s : %s\n' % (word, count))
 
-
-def count_words(filename):
+def count_words(filename, start_depth_level, end_depth_level):
     global counts
     text = open(filename)
 
@@ -41,16 +39,7 @@ def count_words(filename):
             counts[word] = count
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='script merges word pairs from different files')
-    parser.add_argument('input_files_mask', type=str, help='A files mask to merge files')
-    args = parser.parse_args()
-
-    files_mask = args.input_files_mask
-
-    files = glob.glob(files_mask)   # print(files)
-
-    max_words_in_ram = 500
+def merge_files_by_levels(files, output_file_postfix):
     depth_step = math.floor(max_words_in_ram / len(files))
 
     start_depth_level = 0
@@ -58,11 +47,24 @@ if __name__ == "__main__":
 
     while end_depth_level < max_words_in_ram:
         for filename in files:
-            count_words(filename)
+            count_words(filename, start_depth_level, end_depth_level)
 
-        save_to_file(start_depth_level, end_depth_level, filename)
+        save_to_file(start_depth_level, end_depth_level, filename, output_file_postfix)
         start_depth_level = start_depth_level + depth_step
         end_depth_level = end_depth_level + depth_step
         counts.clear()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='script merges word pairs from different files')
+    parser.add_argument('input_files_mask', type=str, help='A files mask to merge files')
+    parser.add_argument('output_file_postfix', type=str, help='A postfix to identify output file based on input file')
+    args = parser.parse_args()
+
+    files_mask = args.input_files_mask
+    output_file_postfix = args.output_file_postfix
+    files = glob.glob(files_mask)
+
+    merge_files_by_levels(files, output_file_postfix)
 
     sys.exit(1)
